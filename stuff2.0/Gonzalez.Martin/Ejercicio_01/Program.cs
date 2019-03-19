aa0baf975442005c7f158c3f858d8eba2a766032
MGonzalezx <40763643+MGonzalezx@users.noreply.github.com>
2019-03-19 12:11:54 -0300

    Add files via upload

---
+++ stuff2.0/Gonzalez.Martin/Ejercicio_01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_01
+{
+    class Ejercicio_01
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_01";
+            int numero;
+            int i;
+            int acumulador;
+            int mayor;
+            int menor;
+            float promedio;
+            for(i=0; i<5; i++)
+            {
+               Console.Write("Ingrese numero: ");
+                numero = int.Parse(Console.ReadLine());
+
+                acumulador = numero;
+                if(i == 0)
+                {
+                    menor = numero;
+                    mayor = numero;
+                }
+                else if (numero > mayor)
+                {
+                    numero = mayor;
+
+                }
+                else if (numero < menor)
+                {
+                    numero = menor;
+                }
+
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ stuff2.0/Gonzalez.Martin/Proyecto01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.WriteLine("Hola Mundo");
+            //System.Console.ReadLine();
+            Console.Write("Ingrese nombre: ");
+            string nombre;
+            nombre = Console.ReadLine();
+           
+            Console.WriteLine("Su nombre es: {0}", nombre);
+            Console.ReadLine();
+        }
+    }
+}
