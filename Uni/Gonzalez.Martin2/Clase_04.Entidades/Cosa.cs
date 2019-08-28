27066dfe56cf279723461a5b024743188bab29f9
MGonzalezx <martingonzalezt97@gmail.com>
2019-08-28 10:47:23 -0300

    Clase_04

---
+++ Uni/Gonzalez.Martin2/Clase_04.Entidades/Cosa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_04.Entidades
+{
+    public class Cosa
+    {
+        private int entero;
+        private string cadena;
+        private DateTime fecha;
+
+        public string Monstrar()
+        {
+            return "Valor entero: " + this.entero.ToString() + " - Cadena: " + this.cadena +
+                " - Fecha: " + this.fecha.ToString() + "\n";
+        }
+
+        public Cosa()
+        {
+            this.entero = -1;
+            this.cadena = " ";
+            this.fecha = DateTime.Now;
+        }
+
+        public Cosa(string str) :this()
+        {
+            //this.entero = -1;
+            this.cadena = str;
+            //this.fecha = DateTime.Now;
+        }
+
+        public Cosa( string str, DateTime fecha) :this(str)
+        {
+            //this.entero = -1;
+            //this.cadena = str;
+            this.fecha = fecha;
+        }
+
+        public Cosa(string str, DateTime fecha, int entero) : this(str, fecha)
+        {
+            //this.fecha = fecha;
+            //this.cadena = str;
+            this.entero = entero;
+        }
+    }
+}
---
+++ Uni/Gonzalez.Martin2/Clase_04/Clase_04.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_04.Entidades;
+
+namespace Clase_04
+{
+    class Clase_04
+    {
+        static void Main(string[] args)
+        {
+            Cosa objeto = new Cosa();
+            Cosa objeto2 = new Cosa("Hola");
+            Cosa objeto3 = new Cosa("Chau", DateTime.Today);
+            Cosa objeto4 = new Cosa("Bien", DateTime.Now, 10);
+
+            Console.Write(objeto.Monstrar());
+            Console.Write(objeto2.Monstrar());
+            Console.Write(objeto3.Monstrar());
+            Console.Write(objeto4.Monstrar());
+
+            Console.ReadKey();
+
+        }
+    }
+}
